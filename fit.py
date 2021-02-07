fb47bd8cf9617c16b503c4454eb82fe6d0506e7d
Kazuki_Kamizuru <kamizuru@k2ss.info>
2021-02-07 16:30:05 +0900

    replace model

--- fit.py
+++ fit.py
@@ -1,16 +1,19 @@
 import os
 import pickle
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 if __name__ == '__main__':
-  df = pd.read_csv(os.environ.get('FILE_PATH'))
+    df = pd.read_csv(os.environ.get('FILE_PATH'))
 
-  df_x = df.drop('winner', axis=1)
-  df_y = df['winner']
+    df_x = df.drop('winner', axis=1)
+    df_y = df['winner']
 
-  clf = MLPClassifier()
-  clf.fit(df_x, df_y)
+    dtc = DecisionTreeClassifier(random_state=42)
+    dtc.fit(df_x, df_y)
+    print(f'scope: {dtc.score(df_x, df_y)}')
 
-  filename = 'finalized_model.sav'
-  pickle.dump(clf, open(filename, 'wb'))
+    filename = 'finalized_model.sav'
+    pickle.dump(dtc, open(filename, 'wb'))
--- predict.py
+++ predict.py
@@ -3,15 +3,17 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 if __name__ == '__main__':
-  filename = 'finalized_model.sav'
-  loaded_model = pickle.load(open(filename, 'rb'))
+    filename = 'finalized_model.sav'
+    loaded_model = pickle.load(open(filename, 'rb'))
 
-  df = pd.DataFrame()
-  # df['player1'] = []
-  # df['player2'] = []
+    df = pd.DataFrame()
+    df['player1'] = []
+    df['player2'] = []
 
-  predicted = loaded_model.predict(df)
-  for i, r in enumerate(predicted):
-    t = df.iloc[i,:]
-    w = t['player1'] if predicted[i] == 1 else t['player2']
-    print(f'{t['player1']}-{t['player2']}: {w} is win.')
+    predicted = loaded_model.predict(df)
+    for i, r in enumerate(predicted):
+        t = df.iloc[i, :]
+        p1 = t['player1']
+        p2 = t['player2']
+        w = t['player1'] if predicted[i] == 1 else t['player2']
+        print(f'{p1}-{p2}: {w} is win.')
